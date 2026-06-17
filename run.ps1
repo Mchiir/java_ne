dbb7ac669ac8582ca977b82cd0e045f031a01f29
Mchrispin <mchrispin14@gmail.com>
2026-06-17 09:10:33 +0200

    add project running powershell script

---
+++ run.ps1
@@ -0,0 +1,7 @@
+# Set environment variables for the application
+$env:JWT_SECRET="Q30J6UziQfsqidqpoxIVd8n6ZYBXx7V1vZ2x89kqDBUDoTAqrUMRkv7Vp64yY4Iiq8GZkqBMcIsIE85XJuJ21D7NHQbvwu9N7zyPQv08aIIqlFcqGhJ4ouRYkJw9bDKL"
+$env:MAIL_USERNAME="mchiirdev@gmail.com"
+$env:MAIL_PASSWORD="dztbfnxzwepmniqc"
+
+# Run the Spring Boot application
+mvn spring-boot:run
